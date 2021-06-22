805f96ea8bd5b5210d9d4ce4b3772f6657420a8e
Akash porwal <62378476+akash7883@users.noreply.github.com>
2021-06-22 22:33:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-window.addEventListener('load',function greet() {
+function greet() {
     const url = (new URL(document.location)).searchParams;
     if (url.get('name') ==' ')
     {
         document.querySelector('h1').innerHTML = "hello, world!";
     }
     document.querySelector('h1').innerHTML = "hello, " + url.get('name') + "!";
-})
+}
